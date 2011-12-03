79b3f9e17f44ef05b7b7d86f06e45ce7b223fe60
Jules Looker <Jules.Looker@me.com>
2011-12-03 13:55:13 +0000

    Initial Commit

---
+++ SeeAlsoInvestigation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SeeAlsoInvestigation
+//
+//  Created by Jules Looker on 03/12/2011.
+//  Copyright (c) 2011 The Interface Works. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
